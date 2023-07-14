40d11cff61b7edd77b7cd662f8bfb5ad6074c5dc
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-07-14 15:01:07 +0300

    Create Greeting componenet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, Route, Routes } from 'react-router-dom'
+import Greetings from "./components/Greetings";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Link to="/greetings">View Greetings</Link>
       <Routes>
         <Route path="/" element={<gg/>} />
-        <Route path="/greetings" element={<gg/>} />
+        <Route path="/greetings" element={<Greetings />} />
       </Routes>
     </div>
   );
