3f341184d31c55d471b4447ff3fb160d757ac4f8
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-07-14 15:51:06 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
-import { Link, Route, Routes } from 'react-router-dom'
-import Greetings from "./components/Greetings";
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import Greetings from './components/Greetings';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       <Link to="/greetings">View Greetings</Link>
       <Routes>
-        <Route path="/" element={<gg/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/greetings" element={<Greetings />} />
       </Routes>
     </div>
--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreetings } from '../redux/reducer';
 
-const Greetings = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greetings);
 
-export default Greetings;
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Link to="/">Back to Home </Link>
+      <p>{greeting.message}</p>
+    </div>
+  );
+};
+
+export default Greeting;
